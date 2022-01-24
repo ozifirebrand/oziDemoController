68aa99ef3a94a97295edebc5c91a316411c26df2
Ozioma <oziomaokoroafor@gmail.com>
2022-01-24 22:44:45 +0100

    Tried reconfiguring ozi controller database

--- src/main/java/africa/ozi/files/data/models/Ozi.java
+++ src/main/java/africa/ozi/files/data/models/Ozi.java
@@ -4,9 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
 @Data
-
 public class Ozi {
     @Id
     private String id;
