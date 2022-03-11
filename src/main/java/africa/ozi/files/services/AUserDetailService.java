16e1abd822f3519230c79bb6cec87e3257a013ef
Ozioma <oziomaokoroafor@gmail.com>
2022-03-11 17:07:13 +0100

    User detail service

--- src/main/java/africa/ozi/files/services/AUserDetailService.java
+++ src/main/java/africa/ozi/files/services/AUserDetailService.java
@@ -1,2 +1,16 @@
-package africa.ozi.files.services;public class AUserDetailService {
+package africa.ozi.files.services;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+@Service
+public class AUserDetailService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User(username, "foo", new ArrayList<>());
+    }
 }
