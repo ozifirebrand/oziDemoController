5fe17c2ba421bc656138e4f303d3d368221f1808
Ozioma <oziomaokoroafor@gmail.com>
2022-03-11 17:06:50 +0100

    Configurer adapter

--- src/main/java/africa/ozi/files/security/CustomWebConfigurerAdapter.java
+++ src/main/java/africa/ozi/files/security/CustomWebConfigurerAdapter.java
@@ -1,2 +1,17 @@
-package africa.ozi.files.security;public class CustomWebConfigurerAdapter {
+package africa.ozi.files.security;
+
+import africa.ozi.files.services.AUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@EnableWebSecurity
+public class CustomWebConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    AUserDetailService myUserDetailsService;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailsService);
+    }
 }
