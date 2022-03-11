f5679a671920d2656a28eec36a4c1a4423bd952b
Ozioma <oziomaokoroafor@gmail.com>
2022-03-11 16:53:38 +0100

    Models

--- src/main/java/africa/ozi/files/data/models/PrinterUser.java
+++ src/main/java/africa/ozi/files/data/models/PrinterUser.java
@@ -1,14 +1,7 @@
 package africa.ozi.files.data.models;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-public class Ozi {
-    @Id
-    private String id;
-    private String firstName;
-    private String lastName;
-    private String password;
+public class PrinterUser {
 }
