99e1924eedf27d8e44883773bb207f9d93a9779b
Ozioma <oziomaokoroafor@gmail.com>
2022-03-15 12:05:44 +0100

    Security config

--- src/main/java/africa/ozi/files/security/CustomWebConfigurerAdapter.java
+++ src/main/java/africa/ozi/files/security/CustomWebConfigurerAdapter.java
@@ -2,13 +2,13 @@ package africa.ozi.files.security;
 
 import africa.ozi.files.services.AUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class CustomWebConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
@@ -20,10 +20,14 @@ public class CustomWebConfigurerAdapter extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(myUserDetailsService);
     }
 
-    @Bean()
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll();
     }
-
-
 }
