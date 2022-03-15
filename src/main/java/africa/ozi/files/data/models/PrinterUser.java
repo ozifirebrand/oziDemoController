a1b26aa65fd3d02afe8c86d0a10dc7b6516df7d0
Ozioma <oziomaokoroafor@gmail.com>
2022-03-15 12:06:03 +0100

    Printer user

--- src/main/java/africa/ozi/files/data/models/PrinterUser.java
+++ src/main/java/africa/ozi/files/data/models/PrinterUser.java
@@ -1,7 +1,9 @@
 package africa.ozi.files.data.models;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
+@Component
 @Data
 public class PrinterUser {
     String firstName;
