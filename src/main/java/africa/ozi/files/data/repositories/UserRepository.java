6a8589e82da15a4d63a06162535ca648ac9279cc
Ozioma <oziomaokoroafor@gmail.com>
2022-03-17 19:28:47 +0100

    Add username Repositories

--- src/main/java/africa/ozi/files/data/repositories/UserRepository.java
+++ src/main/java/africa/ozi/files/data/repositories/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<PrinterUser, Long> {
+    PrinterUser findPrinterUserByUsername(String username);
 }
