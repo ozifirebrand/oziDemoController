6f1af00c56bc15f06aaaf7ba4e7d4feb89bfa196
Ozioma <oziomaokoroafor@gmail.com>
2022-03-17 14:34:53 +0100

    Add security package

--- src/main/java/africa/ozi/files/security/CustomWebConfigurerAdapter.java
+++ src/main/java/africa/ozi/files/security/CustomWebConfigurerAdapter.java
@@ -1,33 +1,83 @@
 package africa.ozi.files.security;
 
+import africa.ozi.files.security.facebook.FacebookConnectionSignup;
+import africa.ozi.files.security.facebook.FacebookSignInAdapter;
 import africa.ozi.files.services.AUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.mem.InMemoryUsersConnectionRepository;
+import org.springframework.social.connect.support.ConnectionFactoryRegistry;
+import org.springframework.social.connect.web.ProviderSignInController;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 
 @Configuration
 @EnableWebSecurity
 public class CustomWebConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    private FacebookConnectionSignup facebookConnectionSignup;
+
+    @Autowired
     AUserDetailService myUserDetailsService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(myUserDetailsService);
+    @Value("${spring.social.facebook.appSecret}")
+    String appSecret;
+    @Value("{spring.social.facebook.appId}")
+    String appId;
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(myUserDetailsService);
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception{
+//        http
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/login*", "/signin/**", "/signup/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin().loginPage("/login").permitAll();
+//    }
+
+    @Bean
+    public ProviderSignInController providerSignInController(){
+        ConnectionFactoryLocator connectionFactoryLocator=
+                connectionFactoryLocator();
+        UsersConnectionRepository usersConnectionRepository =
+                getUsersConnectionRepository(connectionFactoryLocator);
+        ((InMemoryUsersConnectionRepository) usersConnectionRepository)
+                .setConnectionSignUp(facebookConnectionSignup);
+        return new ProviderSignInController(connectionFactoryLocator,
+                usersConnectionRepository, new FacebookSignInAdapter());
+
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/login/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().loginPage("/login").permitAll();
+    private ConnectionFactoryLocator connectionFactoryLocator(){
+        ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
+        registry.addConnectionFactory(new FacebookConnectionFactory(appId
+        ,appSecret));
+        return registry;
+    }
+
+    private UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator){
+        return new InMemoryUsersConnectionRepository(connectionFactoryLocator);
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(myUserDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return provider;
     }
 }
--- src/main/java/africa/ozi/files/security/facebook/FacebookConnectionSignup.java
+++ src/main/java/africa/ozi/files/security/facebook/FacebookConnectionSignup.java
@@ -1,2 +1,25 @@
-package africa.ozi.files.security.facebook;public class FacebookConnectionSignup {
+package africa.ozi.files.security.facebook;
+
+import africa.ozi.files.data.models.PrinterUser;
+import africa.ozi.files.data.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionSignUp;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+@Component
+public class FacebookConnectionSignup implements ConnectionSignUp {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public String execute(Connection<?> connection) {
+        PrinterUser user = new PrinterUser();
+        user.setPassword(randomAlphabetic(8));
+        user.setUsername(connection.getDisplayName());
+        userRepository.save(user);
+        return user.getUsername();
+    }
 }
