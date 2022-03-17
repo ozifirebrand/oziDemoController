0c5d76bb8ca27793f037cb3b73dece2dc999df9a
Ozioma <oziomaokoroafor@gmail.com>
2022-03-17 19:28:13 +0100

    JWT Auth filter

--- src/main/java/africa/ozi/files/filter/JWTAuthorizationFilter.java
+++ src/main/java/africa/ozi/files/filter/JWTAuthorizationFilter.java
@@ -1,2 +1,38 @@
-package africa.ozi.files.filter;public class JWTAuthorizationFilter {
+package africa.ozi.files.filter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+    public JWTAuthorizationFilter(AuthenticationManager authenticationManager){
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain chain) throws ServletException, IOException{
+        String header = request.getHeader("Authorization");
+
+        if ( header == null || !header.startsWith("Bearer ")){
+            chain.doFilter(request, response);
+        }
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(
+                JWT.require(Algorithm.HMAC256("beingsweetandwritingcode@395"))
+                        .build().verify(request.getHeader("Authorization").replace(
+                                "Bearer ", ""
+                        )).getSubject(), null,new ArrayList<>()));
+
+    }
+
 }
