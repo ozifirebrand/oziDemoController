cd78bd4f0e5a7cca7ff9bd7b4ee810a2c2984e6c
Ozioma <oziomaokoroafor@gmail.com>
2022-03-17 14:33:52 +0100

    Add Filter package

--- src/main/java/africa/ozi/files/filter/JWTAuthenticationFilter.java
+++ src/main/java/africa/ozi/files/filter/JWTAuthenticationFilter.java
@@ -1,2 +1,54 @@
-package africa.ozi.files.filter;public class JWTAuthenticationFilter {
+package africa.ozi.files.filter;
+
+import africa.ozi.files.data.models.PrinterUser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+@Slf4j
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+
+    private final ObjectMapper mapper;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager){
+        this.authenticationManager = authenticationManager;
+        this.mapper = new ObjectMapper();
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+        try{
+            PrinterUser user = mapper.readValue(request.getInputStream(),
+                    PrinterUser.class);
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken
+                    (user.getUsername(), user.getPassword());
+            return authenticationManager.authenticate(token);
+        } catch (IOException e) {
+            log.info(e.getMessage());
+        }
+        return super.attemptAuthentication(request,response);
+    }
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain chain, Authentication authenticationResult){
+        String jwtToken = JWT.create().withSubject(((User)authenticationResult.getPrincipal()).getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()))
+                .sign(Algorithm.HMAC256("naMumudeyGoConvocation@yahoo.boy**".getBytes()));
+    }
 }
